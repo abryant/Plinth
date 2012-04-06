a419d982641569272ffdc33ba047b6eac19518c3
Anthony Bryant <anthony@bryants.eu>
2012-04-06 01:44:47 +0100

    Centralise the Usage help text

--- src/eu/bryants/anthony/toylanguage/compiler/Compiler.java
+++ src/eu/bryants/anthony/toylanguage/compiler/Compiler.java
@@ -17,23 +17,25 @@ import eu.bryants.anthony.toylanguage.parser.ToyLanguageParser;
  */
 public class Compiler
 {
+  private static final String USAGE = "Usage: java eu.bryants.anthony.toylanguage.compiler.Compiler <input-file> <output-file>";
+
   public static void main(String... args) throws FileNotFoundException, ParseException, BadTokenException, NameNotResolvedException, ConceptualException
   {
     if (args.length != 2)
     {
-      System.err.println("Usage: java eu.bryants.anthony.toylanguage.compiler.Compiler <input-file> <output-file>");
+      System.err.println(USAGE);
       System.exit(1);
     }
     File input = new File(args[0]);
     File output = new File(args[1]);
     if (!input.isFile())
     {
-      System.err.println("Usage: java eu.bryants.anthony.toylanguage.compiler.Compiler <input-file> <output-file>");
+      System.err.println(USAGE);
       System.exit(2);
     }
     if (output.exists() && !output.isFile())
     {
-      System.err.println("Usage: java eu.bryants.anthony.toylanguage.compiler.Compiler <input-file> <output-file>");
+      System.err.println(USAGE);
       System.exit(3);
     }
 
